e840df962882af391431bd5097967765ab0bf2da
ali-hamza-jutt <128179500+ali-hamza-jutt@users.noreply.github.com>
2026-03-11 23:11:05 +0500

    Created task controller

--- server/src/tasks/tasks.controller.ts
+++ server/src/tasks/tasks.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  UseGuards,
+  UseInterceptors
+} from '@nestjs/common';
 
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+
+
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Tasks')
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+  constructor(private tasksService: TasksService) {}
+
+  @Post()
+  create(@Body() dto: CreateTaskDto) {
+    return this.tasksService.create(dto, "USER_ID");
+  }
+
+  @Get()
+  findAll() {
+    return this.tasksService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.tasksService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() dto: UpdateTaskDto
+  ) {
+    return this.tasksService.update(id, dto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.tasksService.delete(id);
+  }
+
+}
