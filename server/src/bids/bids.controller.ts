3031522acfd0761a229b7a99566f07a2ef51286b
ali-hamza-jutt <128179500+ali-hamza-jutt@users.noreply.github.com>
2026-03-14 21:18:09 +0500

    Created bid controller

--- server/src/bids/bids.controller.ts
+++ server/src/bids/bids.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Delete,
+  Body,
+  Param,
+  UseGuards
+} from '@nestjs/common';
 
+import { BidsService } from './bids.service';
+import { CreateBidDto } from './dto/create-bid.dto';
+
+
+import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+
+@ApiTags('Bids')
+@UseGuards(JwtAuthGuard)
 @Controller('bids')
-export class BidsController {}
+export class BidsController {
+
+  constructor(private bidsService: BidsService) {}
+
+  @Post()
+  create(@Body() dto: CreateBidDto) {
+    return this.bidsService.create(dto, "FREELANCER_ID");
+  }
+
+  @Get('task/:taskId')
+  findByTask(@Param('taskId') taskId: string) {
+    return this.bidsService.findByTask(taskId);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.bidsService.delete(id);
+  }
+
+}
