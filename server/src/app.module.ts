f311a7e0ac52770e6edc9b944be2e8aa050fa076
ali-hamza-jutt <128179500+ali-hamza-jutt@users.noreply.github.com>
2026-03-14 19:48:30 +0500

    Added user module in app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -16,7 +17,8 @@ import { TasksModule } from './tasks/tasks.module';
       process.env.MONGODB_URI || 'mongodb://localhost:27017/marketplace',
     ),
     AuthModule,
-    TasksModule
+    TasksModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
