b0bd55e6b1c0cbac919f5f98e47cde19639576a4
ali-hamza-jutt <128179500+ali-hamza-jutt@users.noreply.github.com>
2026-03-14 21:13:51 +0500

    created schema for bid module

---
+++ server/src/bids/schemas/bid.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type BidDocument = Bid & Document;
+
+@Schema({ timestamps: true })
+export class Bid {
+
+  @Prop({ required: true })
+  taskId: string;
+
+  @Prop({ required: true })
+  freelancerId: string;
+
+  @Prop({ required: true })
+  bidAmount: number;
+
+  @Prop()
+  message: string;
+
+}
+
+export const BidSchema = SchemaFactory.createForClass(Bid);
